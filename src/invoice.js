da08a1f82cb7aa6219393a43e02a4e12c61f44a6
Jack13-Han <hanwaihtun1e.com>
2026-05-21 09:12:46 +0900

    First Commit

---
+++ src/invoice.js
@@ -0,0 +1,7 @@
+class Invoice {
+    init(){
+        console.log("Invoice app Started");
+    }
+}
+
+export default Invoice;
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// import "./style.css";
+// import Invoice from "./invoice";
+
+
+
+// const invoice = new Invoice();
+// invoice.init();
+
+// // Flowbite initialize (for dynamic components)
+// initFlowbite();
