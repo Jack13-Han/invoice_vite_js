fdedc0192b2fa84af68ea93d967d2cacb0fe2760
Jack13-Han <hanwaihtun1e.com>
2026-05-24 00:18:34 +0900

    add table

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-// import "./style.css";
-// import Invoice from "./invoice";
+import "./style.css";
+import("preline")
+  .then((mod) => {
+    const Preline = mod.default || mod;
+    if (Preline && typeof Preline.init === "function") Preline.init();
+  })
+  .catch(() => {
+    // Preline failed to load; continue silently
+  });
 
-
-
+// import Invoice from './invoice';
 // const invoice = new Invoice();
 // invoice.init();
-
-// // Flowbite initialize (for dynamic components)
-// initFlowbite();
